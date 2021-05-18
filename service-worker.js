61d7a7519d4986f503413a8bf37ca1a184933e01
Anton Kostylov <antoniok003@gmail.com>
2021-05-18 07:51:51 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.18c9f0d40b081d257cd4f081734f508a.js"
 );
 
 self.addEventListener('message', (event) => {
